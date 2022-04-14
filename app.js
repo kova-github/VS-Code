15f0420d04c5fceabddd1fbf94ef9d0b82696026
Nastya Kovaleva <59012012+kova-github@users.noreply.github.com>
2022-04-14 09:49:09 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+let yolochka1 = 20;
+let yolochka2 = "Hello";
+let result = yolochka1 + yolochka2;
+console.log(yolochka1);
+console.log(yolochka2);
+console.log(result);
+let n1 = 10,
+    n2 = 30,
+    yolochka3 = 100,
+    n4 = 200;
+
+let let_type1 = typeof(yolochka3);
+let let_type2 = typeof(yolochka2);
+let bool_true = true;
+let bool_false = false;
+console.log(let_type1)
+console.log(let_type2)
